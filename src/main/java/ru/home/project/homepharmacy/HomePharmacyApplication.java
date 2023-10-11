3ce21c8160322ab1b03af10a8c47eab1f2cad96a
Sergey Solin <livegrafics@rambler.ru>
2023-10-11 13:39:24 +0300

    Initial commit

---
+++ src/main/java/ru/home/project/homepharmacy/HomePharmacyApplication.java
@@ -0,0 +1,13 @@
+package ru.home.project.homepharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomePharmacyApplication.class, args);
+    }
+
+}
